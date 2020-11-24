0b370e1cac5b33d679f9957a0ed31c831d25d8db
zyzyzyf <2449392866@qq.com>
2020-11-24 15:51:02 +0800

    commit

---
+++ src/week6/CircleWithPrivateDataFields.java
@@ -0,0 +1,24 @@
+package week6;
+public class CircleWithPrivateDataFields {
+	private double radius=1;
+	private static int numberOfObjects=0;
+	public CircleWithPrivateDataFields(){
+		numberOfObjects++;
+	}
+	public CircleWithPrivateDataFields(double newRadius){
+		radius=newRadius;
+		numberOfObjects++;
+	}	
+	public double getRadius(){
+		return radius;
+	}
+	public void setRadius(double newRadius){
+		radius=(newRadius>=0)?newRadius:0;
+	}
+	private static int getNumberOfObjects(){
+		return numberOfObjects;
+	}
+	public double getArea(){
+		return radius*radius*Math.PI;
+	}
+}
---
+++ src/week6/CircleWithStaticMembers.java
@@ -0,0 +1,20 @@
+package week6;
+
+public class CircleWithStaticMembers {
+	double radius;
+	static int numberOfObjects=0;
+	CircleWithStaticMembers(){
+		radius=1;
+		numberOfObjects++;
+	}
+	CircleWithStaticMembers(double newRadius){
+		radius=newRadius;
+		numberOfObjects++;
+	}
+	static int getNumberOfObjects(){
+		return numberOfObjects;
+	}
+	double getArea(){
+		return radius*radius*Math.PI;
+	}
+}
